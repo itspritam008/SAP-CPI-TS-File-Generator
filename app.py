076d68edbc5a5fbe70a107d4d5dfce144ab60ca5
Pritam Paul <pritampaul2k17@gmail.com>
2026-08-04 12:52:46 +0530

    Changed in the naming convention

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def generate():
         # Generate DOCX document
         generated_docx_path = build_visteon_ts_docx(parsed_data, output_dir=temp_dir)
 
-        download_name = f"Technical Specifications_{parsed_data['iflow_name']}.docx"
+        download_name = f"TS_{parsed_data['iflow_name']}.docx"
 
         # Send generated file back to user
         response = send_file(
